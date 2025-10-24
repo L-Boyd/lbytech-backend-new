56aae40836692ed2b56d4deb2373332e8f6e1177
lby-magicbook <1508201988@qq.com>
2025-10-24 12:32:11 +0800

    feat：基础响应工具类

---
+++ src/main/java/com/lbytech/lbytech_backend_new/util/ResultUtil.java
@@ -0,0 +1,29 @@
+package com.lbytech.lbytech_backend_new.util;
+
+import com.lbytech.lbytech_backend_new.pojo.Enum.StatusCodeEnum;
+import com.lbytech.lbytech_backend_new.pojo.vo.BaseResponse;
+
+public class ResultUtil {
+
+        /**
+         * 成功
+         *
+         * @param data
+         * @param <T>
+         * @return
+         */
+        public static <T> BaseResponse<T> success(T data) {
+            return new BaseResponse<>(StatusCodeEnum.SUCCESS, data);
+        }
+
+        /**
+         * 失败
+         *
+         * @param data
+         * @param <T>
+         * @return
+         */
+        public static <T> BaseResponse<T> fail(T data) {
+            return new BaseResponse<>(StatusCodeEnum.FAIL, data);
+        }
+}
