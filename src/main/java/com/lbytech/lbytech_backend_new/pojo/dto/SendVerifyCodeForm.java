d8b6817e9805af4f073d90ffced3695e1a3150c6
lby-magicbook <1508201988@qq.com>
2025-10-24 15:36:53 +0800

    fix：去掉notnull

--- src/main/java/com/lbytech/lbytech_backend_new/pojo/dto/SendVerifyCodeForm.java
+++ src/main/java/com/lbytech/lbytech_backend_new/pojo/dto/SendVerifyCodeForm.java
@@ -1,7 +1,6 @@
 package com.lbytech.lbytech_backend_new.pojo.dto;
 
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * 发送验证码表单
@@ -9,7 +8,6 @@ import org.jetbrains.annotations.NotNull;
 @Data
 public class SendVerifyCodeForm {
 
-    @NotNull
     // 邮箱
     String email;
 
