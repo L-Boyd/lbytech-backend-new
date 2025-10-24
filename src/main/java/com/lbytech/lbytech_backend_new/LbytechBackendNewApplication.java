1cd671ad40c1d32ecf0727649c67cc27e2dd4d49
lby-magicbook <1508201988@qq.com>
2025-10-24 10:57:40 +0800

    spring-boot项目初始化

---
+++ src/main/java/com/lbytech/lbytech_backend_new/LbytechBackendNewApplication.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbytech_backend_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbytechBackendNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbytechBackendNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbytech/lbytech_backend_new/LbytechBackendNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbytech.lbytech_backend_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbytechBackendNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
