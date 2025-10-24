fdfd018de97c223af50c63e81786be32ccb3c893
lby-magicbook <1508201988@qq.com>
2025-10-24 15:14:52 +0800

    fix:用户实体类缩进、构造器

--- src/main/java/com/lbytech/lbytech_backend_new/pojo/entity/User.java
+++ src/main/java/com/lbytech/lbytech_backend_new/pojo/entity/User.java
@@ -6,15 +6,19 @@ import lombok.Data;
 /**
  * 用户实体类
  */
-@AllArgsConstructor
 @Data
 public class User {
-        // 主键
-        private Long id;
+    // 主键
+    private Long id;
 
-        // 邮箱
-        private String email;
+    // 邮箱
+    private String email;
 
-        // 密码
-        private String password;
+    // 密码
+    private String password;
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
 }
