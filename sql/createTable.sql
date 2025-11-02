75c88790d661f4b49cbc9118212cfd19e6317a6f
lby-magicbook <1508201988@qq.com>
2025-11-02 16:56:16 +0800

    feat：用户表新增创建时间字段，创建笔记表

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -5,6 +5,17 @@ create table User
     id       int auto_increment comment '用户id'
         primary key,
     email    varchar(255) not null comment '用户邮箱',
-    password varchar(255) not null comment '用户密码'
+    password varchar(255) not null comment '用户密码',
+    create_time datetime not null comment '创建时间'
 )
     comment '用户表';
+
+create table file
+(
+    id          int      auto_increment comment '笔记id'
+        primary key,
+    file_name   varchar(255) not null comment '笔记名',
+    file_url    text null comment '笔记地址',
+    create_time datetime not null comment '创建时间'
+)
+    comment '笔记表';
