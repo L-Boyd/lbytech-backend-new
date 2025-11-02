7d546678ab0510b5471a69e305e9800321faa7e0
lby-magicbook <1508201988@qq.com>
2025-11-02 20:04:19 +0800

    fix：User添加无参构造器，解决转换失败问题

--- src/main/java/com/lbytech/lbytech_backend_new/pojo/entity/User.java
+++ src/main/java/com/lbytech/lbytech_backend_new/pojo/entity/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
  * 用户实体类
  */
 @Data
+@NoArgsConstructor  // 没有无参构造函数的话，会尝试把password往localDateTime映射然后报错。有无参构造函数，mybatis会用无参+setter来转换。
 public class User {
     // 主键，使用数据库自增
     @TableId(type = IdType.AUTO)
