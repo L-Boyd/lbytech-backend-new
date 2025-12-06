9e61eee6a1b115df9d9f099010be3a9784eac07e
lby-magicbook <1508201988@qq.com>
2025-12-06 16:13:52 +0800

    feat:更新数据库笔记表：新增‘点赞数’字段，添加‘’更新时间字段，完善‘创建时间’字段

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -19,3 +19,13 @@ create table notebook
     create_time datetime not null comment '创建时间'
 )
     comment '笔记表';
+
+alter table notebook
+    add thumb_count int default 0 not null comment '点赞数' after file_url;
+
+alter table notebook
+    alter column create_time set default (CURRENT_TIMESTAMP);
+
+alter table notebook
+    add update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间';
+
