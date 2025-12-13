17e4fcb3908d2e2e60842b02a6fedd7ebe701069
lby-magicbook <1508201988@qq.com>
2025-12-13 11:28:20 +0800

    feat:lua脚本执行状态枚举、点赞类型枚举

---
+++ src/main/java/com/lbytech/lbytech_backend_new/pojo/Enum/LuaStatusEnum.java
@@ -0,0 +1,21 @@
+package com.lbytech.lbytech_backend_new.pojo.Enum;
+
+import lombok.Getter;
+
+/**
+ * Lua脚本执行状态枚举
+  */
+@Getter
+public enum LuaStatusEnum {  
+    // 成功  
+    SUCCESS(1L),
+    // 失败  
+    FAIL(-1L),
+    ;  
+  
+    private final long value;
+  
+    LuaStatusEnum(long value) {
+        this.value = value;  
+    }  
+}
