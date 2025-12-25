b6520b6880a7f0bf11098bda655878f64cd99b19
lby-magicbook <1508201988@qq.com>
2025-12-25 16:52:14 +0800

    debug

--- src/main/java/com/lbytech/lbytech_backend_new/manager/cache/topK/AddResult.java
+++ src/main/java/com/lbytech/lbytech_backend_new/manager/cache/topK/AddResult.java
@@ -3,7 +3,7 @@ package com.lbytech.lbytech_backend_new.manager.cache.topK;
 import lombok.Data;
 
 @Data
-class AddResult {
+public class AddResult {
     // 被挤出的 key
     private final String expelledKey;
     // 当前 key 是否进入 TopK
