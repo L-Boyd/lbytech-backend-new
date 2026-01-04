5ed12abaf75eabf635175a72132badb5eb46a1e3
lby-magicbook <1508201988@qq.com>
2026-01-04 16:26:35 +0800

    feat：引入prometheus+grafana，添加关键业务指标（点赞成功、失败数）

--- src/main/java/com/lbytech/lbytech_backend_new/controller/ThumbController.java
+++ src/main/java/com/lbytech/lbytech_backend_new/controller/ThumbController.java
@@ -4,6 +4,8 @@ import com.lbytech.lbytech_backend_new.pojo.dto.ThumbRequest;
 import com.lbytech.lbytech_backend_new.pojo.vo.BaseResponse;
 import com.lbytech.lbytech_backend_new.service.IThumbRecordService;
 import com.lbytech.lbytech_backend_new.util.ResultUtil;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +19,37 @@ public class ThumbController {
     @Autowired
     private IThumbRecordService thumbRecordService;
 
+    // 计数器：记录点赞成功和失败的次数
+    private final Counter successCounter;
+    private final Counter failureCounter;
+    public ThumbController(MeterRegistry meterRegistry) {
+        this.successCounter = Counter.builder("thumb.success.count")
+                .description("Total successful thumb")
+                .register(meterRegistry);
+        this.failureCounter = Counter.builder("thumb.failure.count")
+                .description("Total failed thumb")
+                .register(meterRegistry);
+    }
+
     @PostMapping("/thumbNotebook")
     @Operation(summary = "点赞笔记本")   // springdoc描述方法
     public BaseResponse<Boolean> thumbNotebook(@RequestBody ThumbRequest thumbRequest) {
-        Boolean success = thumbRecordService.thumbNotebook(thumbRequest.getNotebookId());
-        return ResultUtil.success(success);
+        try {
+            Boolean success = thumbRecordService.thumbNotebook(thumbRequest.getNotebookId());
+            if (success) {
+                // 成功计数
+                successCounter.increment();
+                return ResultUtil.success(true);
+            } else {
+                // 失败计数
+                failureCounter.increment();
+                return ResultUtil.fail(false);
+            }
+        } catch (Exception e) {
+            // 失败计数
+            failureCounter.increment();
+            throw new RuntimeException(e);
+        }
     }
 
     @PostMapping("/unThumbNotebook")
