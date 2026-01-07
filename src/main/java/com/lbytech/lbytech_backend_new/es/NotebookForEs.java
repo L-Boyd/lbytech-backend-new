56c52b6a94ecb4e39864fffad3681d790dbcea2d
lby-magicbook <1508201988@qq.com>
2026-01-07 19:07:56 +0800

    feat:引入es，配置es，notebook的es实体类

---
+++ src/main/java/com/lbytech/lbytech_backend_new/es/NotebookForEs.java
@@ -0,0 +1,36 @@
+package com.lbytech.lbytech_backend_new.es;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+// es索引名称为notebook，自动创建索引
+@Document(indexName = "notebook", createIndex = true)
+@Data
+public class NotebookForEs {
+
+    // ES 文档ID
+    @Id
+    private Integer id;
+
+    // 字段映射：name，类型text（可分词），analyzer：分词器（ik_max_word是IK中文分词器）
+    @Field(name = "file_name", type = FieldType.Text, analyzer = "ik_max_word")
+    private String fileName;
+
+    // 笔记内容
+    @Field(name = "content", type = FieldType.Text, analyzer = "ik_max_word")
+    private String content;
+
+    @Field(name = "thumb_count", type = FieldType.Integer)
+    private Integer thumbCount;
+
+    @Field(name = "create_time", type = FieldType.Date, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    @Field(name = "update_time", type = FieldType.Date, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+}
